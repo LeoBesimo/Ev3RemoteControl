eef406d4548a7b435431a43a6b0b2ca4c5524fa5
Leo Besimo <besimol515@gmail.com>
2023-08-10 00:32:54 +0200

    working client server connection and drivable with a controller

--- client.py
+++ client.py
@@ -2,21 +2,34 @@ import socket
 import threading
 from Controller import ControllerHandler, InputMethods
 
-inputValues = {}
+inputValues = {
+    InputMethods.LStickY.name: 0,
+    InputMethods.RStickY.name: 0
+}
 
 handler = ControllerHandler()
 serverPort = 55555
+
+threshhold = 3000
+
+def map(value, oldMin, oldMax, newMin, newMax):
+    return newMin + (newMax - newMin) * ((value - oldMin) / (oldMax - oldMin))
 
 def getControls():
     while True:
         inputs = handler.getInputs()
         for input in inputs:
-            inputValues[input[0].name] = input[1]
+            if input[0] == InputMethods.LStickY or input[0] == InputMethods.RStickY:
+                if(input[1] > -threshhold and input[1] < threshhold):
+                    inputValues[input[0].name] = 0
+                else:
+                    inputValues[input[0].name] = int(map(input[1],-32767, 32767,-100,100))
         
 
 def main():
-    #serverIP = input("Input Server IP: ")
-    serverIP = "192.168.1.38"
+    serverIP = input("Input Server IP: ")
+    #serverIP = "192.168.1.38"
+    #serverIP = "192.168.1.18"
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((serverIP, serverPort))
     print(client.recv(1024).decode("utf-8"))
@@ -29,8 +42,9 @@ def main():
 
     while True:
         try:
-            client.send(f"{inputValues}".encode("utf-8"))
-            print(client.recv(1024).decode("utf-8"))
+            client.send(f"{inputValues[InputMethods.LStickY.name]}:{inputValues[InputMethods.RStickY.name]}".encode("utf-8"))
+            client.recv(1024)
+            #print(client.recv(1024).decode("utf-8"))
         except Exception as e:
             print(e)
             break
--- server.py
+++ server.py
@@ -1,33 +1,72 @@
+#!/usr/bin/env python3
+
 import socket
 import threading
+from time import sleep
+import os
+
+from ev3dev2.motor import MoveTank, OUTPUT_B, OUTPUT_C
 
 serverPort = 55555
+lSpeed = 0
+rSpeed = 0
+
+motor = MoveTank(OUTPUT_C, OUTPUT_B)
 
 def connectClient(server):
     server.listen()
     client, clientIP = server.accept()
-    print(f"Client Connected with IP {clientIP}")
+    print("Client Connected")
+    print("Client Connected with IP: ")
+    print(str(clientIP))
     return client
 
+def parseMessage(message):
+    global rSpeed, lSpeed
+    splitMessage = message.split(":")
+    rSpeed = int(splitMessage[1])
+    lSpeed = int(splitMessage[0])
+    #print("RSpeed: " + str(rSpeed) + "   LSpeed: " + str(lSpeed))
 
+def driveRobot():
+    global lSpeed, rSpeed, motor
+    while True:
+        #print(str(lSpeed) + " " + str(rSpeed))
+        if(lSpeed == 0 and rSpeed == 0):
+            motor.off()
+        else:
+            motor.on(left_speed=lSpeed, right_speed=rSpeed)
 
 def main():
+
+    os.system('setfont Lat15-TerminusBold14')
+
     hostname = socket.gethostname()
     ipaddr = socket.gethostbyname(hostname)
     
-    print(f"hostname: {hostname}    ip: {ipaddr}")
-    
+    #print(f"hostname: {hostname}    ip: {ipaddr}")
+    print("Hostname: " + hostname + "   ip: " + ipaddr)
+
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((ipaddr, serverPort))
     
     client = connectClient(server)
 
+    moveThread = threading.Thread(target=driveRobot)
+    moveThread.start()
+
     while True:
         try:
-            client.send(f"server".encode("utf-8"))
-            print(client.recv(1024).decode("utf-8"))
+            #sleep(0.01)
+            client.send("Confirmed".encode("utf-8"))
+            message = client.recv(1024).decode("utf-8")
+            parseMessage(message)
+            #print(message)
         except Exception as e:
-            print(e)
+            #print("Client Disconnected due to: ")
+            #print(e)
+            print("Client Disconnected")
+            print("Hostname: " + hostname + "   ip: " + ipaddr)
             client = connectClient(server)
 
 
